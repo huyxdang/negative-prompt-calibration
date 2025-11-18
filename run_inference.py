568d46d3dfd4e629e53249def99fb77eb418b7d2
Huy Dang <xhuydng@gmail.com>
2025-11-18 20:22:48 +0700

    m

--- run_inference.py
+++ run_inference.py
@@ -311,7 +311,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--batch_size",
         type=int,
-        default=50,
+        default=150,
         help="Batch size for processing"
     )
     
